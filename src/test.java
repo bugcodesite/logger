3c847a8bc53e38e46fc735c35c86700459d2b25e
bugecode <373335273@qq.com>
2022-11-30 07:36:53 +0000

    test

---
+++ src/test.java
@@ -0,0 +1,8 @@
+import site.bc.logger.Logger;
+public  class test{
+    public static void main(String...args){
+        Logger.config("/var/log/test");
+        Logger.out("ok");
+        System.out.println("test end,please see "+"/var/log/test");
+    }
+}
